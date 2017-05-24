8f98d611baacb5ddc62e602a5665f0ddf92d4057
Sam <Sam@Sams-MacBook-Pro.local>
2017-05-24 13:41:52 +1000

    Added middleware to JobsController and made exceptions for two methods on controller

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -10,6 +10,16 @@ use Carbon\Carbon;
 
 class JobsController extends Controller
 {
+
+
+
+        public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+
+
     /**
      * Display a listing of the resource.
      *
