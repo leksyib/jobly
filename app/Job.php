86d4b56260581d0294f5d79f5e128d5fb1ca8c56
Sam <Sam@Sams-MacBook-Pro.local>
2017-05-24 16:56:39 +1000

    Added dynmaic methods to models

--- app/Job.php
+++ app/Job.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    //
+    public function applications(){
+    	return $this->hasMany('App\Application');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,52 @@
                 <div class="panel-heading">Your Job Listings</div>
 
                 <div class="panel-body">
+                                    <table class="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                Job Title
+                            </th>
+                            <th>
+                                Applications
+                            </th>
+                            <th>
+                                Status
+                            </th>
+                            <th>
+                            </th>
+                            <th>
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
                     @foreach($jobs as $job)
+                        <tr>
+                            <td>
+                            {{$job->title}}
+                            </td>
+                            <td>
+                             
+                            </td>
+                            <td>
+                                
+                            </td>
+                            <td>
+                                <a href="">View Applications</a>
+                            </td>
+                            <td>
+                                <a href="">Edit Job</a>
+                            </td>
+                            <td>
+                                <a href="">Delete Job</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
 
-                    <a href="jobs/{{$job->id}}">{{$job->title}}</a> <a href="">View Applications</a></br>
-                    @endforeach
                 </div>
             </div>
         </div>
